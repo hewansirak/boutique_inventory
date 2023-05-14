2148a7128c2b28e6b56e94ad171892ee9b160056
nigussolomon <nigussolomon444@gmail.com>
2023-05-14 08:43:31 +0300

    potential fix

--- boutique/models.py
+++ boutique/models.py
@@ -28,6 +28,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     role = models.CharField(max_length=20, choices=ROLES)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
