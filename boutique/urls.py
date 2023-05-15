5be46ddcc3a552569ce6bd83d55142240767f7e3
nigussolomon <nigussolomon444@gmail.com>
2023-05-15 12:35:07 +0300

    potential solution

---
+++ boutique/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('login/', loginView.as_view(), name='login'),
+    path('home/', Home, name='home'),
+    path('admin_dash/', Home, name='admin_dashboard'),
+    path('sales/', sales_view, name='sales'),
+    path('stock_count/', sales_view, name='stock_count'),
+    path('logout/', sales_view, name='logout'),
+]
